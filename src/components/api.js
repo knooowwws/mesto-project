8e5876e238fe483f98020437b806d41091858d63
knooowwws <khangereev2001@mail.ru>
2021-10-30 22:09:49 +0400

    add Constant

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,3 @@
-const config = {
-    baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-2',
-    headers: {
-        authorization: 'f8b50c22-f9ea-4450-b40d-792adb8f1524',
-        'Content-Type': 'application/json'
-    }
-}
 
 export const getResponse = (res) => {
     if (res.ok) {
---
+++ src/components/constants.js
@@ -0,0 +1,7 @@
+const config = {
+    baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-2',
+    headers: {
+        authorization: 'f8b50c22-f9ea-4450-b40d-792adb8f1524',
+        'Content-Type': 'application/json'
+    }
+}
