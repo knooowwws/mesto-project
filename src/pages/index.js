a6b7c60c1661e25da1201fe0fdfd5c8dc890eb1b
LiubovMavlieva <mavlieva13@icloud.com>
2021-11-06 21:23:13 +0300

    index2.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import PopupWithForm from "../components/popupWithForm"
 import Section from "../components/Section"
 import { UserInfo } from "../components/UserInfo"
 import FormValidator from "../components/validate"
-import {config, profileName, profileWork, profileAvatar} from "../components/constants"
+import {config, profileName, profileWork, profileAvatar, template, cardSection , photoModal} from "../components/constants"
 
 let myId
 let section
@@ -17,21 +17,36 @@ function getId(id) {
 const api = new Api (config.baseUrl , config.headers)
 const userInfo = new UserInfo('profile__name', 'profile__work-place', api)
 
+const cardRender = (item) => {
+	let liked = false;
+	let myCard = false;
+	item.likes.forEach(function (el) {
+		if (el._id === myId) {
+			liked = true;
+		}
+	});
+	if (item.owner._id === myId) {
+		myCard = true;
+	}
+	const card = new Card(item, template, api, liked, myCard, photoModal.open.bind(photoModal));
+	card.addCard(template, cardSection);
+};
 
-Promise.all([userInfo.getUserInfo(), api.getInitialCards()]).then(([userData, cards] => {
+Promise.all([userInfo.getUserInfo(), api.getInitialCards()]).then(([userData, cards]) => {
     getId(userData._id)
     profileName.textContent = userData.name
     profileWork.textContent = userData.about
     profileAvatar.textContent = userData.avatar
     section = new Section({
         items: cards,
-        renderer: (item) => {
-
+        renderer: function (item) {
+         cardRender(item);
         }
-    })
+    }, cardSection);
+   section.renderItems();
 })
 
-const popUpWithImg = new PopupWithImg('.popup__photo')
+const popUpWithImg = new PopupWithImg(photoModal)
 
 
 //вызовы
