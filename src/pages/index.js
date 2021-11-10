fc4cb1ba9442e51d484e0b2fe9a1896d5011315f
knooowwws <khangereev2001@mail.ru>
2021-11-10 18:37:28 +0400

    index__change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,111 @@
 import './index.css';
-
 import { Api } from "../components/api";
 import {Card} from "../components/newCard"
-import PopUp from "../components/PopUp"
-import {PopupWithImg} from "../components/popupWithImg"
-import {PopupWithForm} from "../components/popupWithForm"
+import { Popup } from '../components/Popup';
+import {PopupWithImage} from "../components/popupWithImg"
+import { PopupWithForm } from '../components/popupWithForm';
 import {Section} from "../components/Section"
 import { UserInfo } from "../components/UserInfo"
-import FormValidator from "../components/validate"
-import {config, profileName, profileWork, profileAvatar,modalProfile, template, cardSection , photoModal} from "../components/constants"
-
-let myId
-let section
-function getId(id) {
-    myId = id
+import {FormValidate} from "../components/validate"
+import {config , cardConfig , profile , avatar , imagePreviewConfig, popupEditConfig , popupPhoto , nameInput, aboutInput, profileAvatar, popupAddConfig , validateConfig, buttons} from "../components/constants"
+//api
+const api = new Api(config)
+// Модальные окна +
+const popupWithImg = new PopupWithImage(popupPhoto);
+const popupAdd = new PopupWithForm(popupAddConfig.popupAdd, {
+    submitHandler: (data) => {
+        buttons.add.textContent = 'Сохранение ...';
+        api.addNewCard(data.place, data.link)
+            .then(result => {
+                const addCard = createCard(result);
+                section.addItem(addCard, 'prepend');
+                popupAdd.close();
+            })
+            .catch(result => console.log(`${result} при отправке карточки`))
+            .finally(() => {
+                buttons.add.textContent = 'Сохранить'
+            })
+    }
+});
+const popupEdit = new PopupWithForm(popupEditConfig.editPopup, {
+    submitHandler: (data) => {
+        buttons.edit.textContent = 'Сохранение ...';
+        api.saveProfileData(data.name, data.about)
+            .then(result => {
+                userInfo.setUserInfo(result.name, result.about)
+                popupEdit.close()
+            })
+            .catch(result => console.log(`${result} при отправке данных пользователя`))
+            .finally(() => {
+                buttons.edit.textContent = 'Сохранить'
+            })
+    }
+});
+// Вызов модальных окон +
+popupWithImg.setEventListeners();
+popupAdd.setEventListeners();
+popupAddConfig.addButton.addEventListener('click', () => {
+    popupAdd.open()
+});
+popupEdit.setEventListeners();
+function handlePopupEdit() {
+    const profileInfo = userInfo.getUserInfo();
+    popupEditConfig.nameInput.value = profileInfo.name;
+    popupEditConfig.jobInput.value = profileInfo.about;
+    formEditValidate.clearValidationState();
+    popupEdit.open();
 }
-
-const popUpWithImg = new PopupWithImg('.popup_photo')
-const api = new Api (config)
-const userInfo = new UserInfo('profile__name', 'profile__work-place', api)
-
-const cardRender = (item) => {
-	let liked = false;
-	let myCard = false;
-	item.likes.forEach(function (el) {
-		if (el._id === myId) {
-			liked = true;
-		}
-	});
-	if (item.owner._id === myId) {
-		myCard = true;
-	}
-	const card = new Card(item, api, liked, myCard, (evt) => {
-	    console.log(card)
-
-
-    });
-	card.addCard(template, cardSection);
-
-};
-
-Promise.all([api.getUserProfile(), api.getInitialCards()]).then(([userData, cards]) => {
-    getId(userData._id)
-    profileName.textContent = userData.name
-    profileWork.textContent = userData.about
-    profileAvatar.src = userData.avatar
-    section = new Section({
-        items: cards,
-        renderer: function (item) {
-         cardRender(item);
-        }
-    }, cardSection);
-   section.renderItems();
-})
-
-
-// const popupFormEditProfile = new PopupWithForm(modalProfile, (newData) => {
-//     // popupFormEditProfile.renderLoading(true)
-//     api.saveProfileData(newData.name , newData.about)
-//       .then((res) => {
-//         userInfo.setUserInfo(res)
-//         popupFormEditProfile.close()
-//       })
-//       .catch((err) => console.log(err))
-//     //   .finally(() => popupFormEditProfile.renderLoading(false))
+popupEditConfig.editBtn.addEventListener('click', handlePopupEdit);
+// user Info -
+const userInfo = new UserInfo(popupEditConfig.nameInfo, popupEditConfig.jobInfo, profileAvatar);  //При изменении объекта на простую константу - всё ломается
+// Section +
+const section = new Section({
+    renderer: (item) => {
+        const newCard = createCard(item);
+        section.addItem(newCard, 'append');
+    }
+}, '.cards');
+// Create Card
+function createCard(item) {
+    const userId = userInfo.getUserId()
+    const element = new Card(item, {
+            handleImageClick: (link, alt) => {
+                popupWithImg.open({link, alt});
+            },
+            toggleLike: (cardId) => {
+                if (cardId.querySelector(cardConfig.elementLike).classList.contains(cardConfig.LikeActive)) {
+                    api.dislikeCard(cardId.id)
+                        .then(result => element.removeLike(cardId, result.likes))
+                        .catch(result => console.log(`${result} при удалении лайка`))
+                } else {
+                    api.likeCard(cardId.id)
+                        .then(result => element.addLike(cardId, result.likes))
+                        .catch(result => console.log(`${result} при удалении лайка`))
+                }
+            }
+        },
+        cardConfig, '.card', userId, api);
+    const card = element.generateCard();
+    return card;
+}
+// Promise All
+Promise.all([api.getUserProfile(), api.getInitialCards()])
+    .then(([userData, cards]) => {
+        userInfo.setUserInfo(userData.name, userData.about, userData._id);
+        userInfo.setUserAvatar(userData.avatar);
+        section.renderItems(cards)
+        return section;
+    })
+//   .catch(result => console.log(`${result} при загрузке данных`))
+//   .finally(function () {
+//     whileLoading.profile.style.display = 'flex';
+//     whileLoading.footer.style.display = 'flex';
+//     whileLoading.loading.style.display = 'none';
 //   })
-//   popupFormEditProfile.setEventListeners();
-
-
-popUpWithImg.setEventListeners()
-
+// Validation
+const formEditValidate = new FormValidate(validateConfig, popupEditConfig.editPopup);
+formEditValidate.enableValidation();
+const formAddValidate = new FormValidate(validateConfig, popupAddConfig.popupAdd);
+formAddValidate.enableValidation();
+const formAvatarValidate = new FormValidate(validateConfig, avatar)
+formAvatarValidate.enableValidation();
